b8fa26c9fee1c0805e5536b6059250f7a942d111
Ashok Yadav <yadav.ashok41@gmail.com>
2023-12-20 17:57:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/todolistapp/task_database/TaskDatabaseHelper.java
@@ -0,0 +1,30 @@
+package com.example.todolistapp.task_database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TaskDatabaseHelper extends SQLiteOpenHelper {
+
+    public TaskDatabaseHelper(Context context) {
+        super(context, TaskContract.DB_NAME, null, TaskContract.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TaskContract.TaskEntry.TABLE + " ( " +
+                TaskContract.TaskEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                TaskContract.TaskEntry.COL_TASK_TITLE + " TEXT NOT NULL);";
+
+        //Above statement is equivalent to
+        //String createTable = "CREATE TABLE Tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, Title TEXT NOT NULL);";
+
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TaskContract.TaskEntry.TABLE);
+        onCreate(db);
+    }
+}
